6bb9e638ee9693b10b7ed9c5a42c4afb9c31d74b
Aelxei Buravcev <alexei.buravcev@gmail.com>
2021-08-19 11:31:33 +0300

    Add files via upload

--- Sections/Array/Find pair with given sum in the array/Brute-Force/Program.cs
+++ Sections/Array/Find pair with given sum in the array/Brute-Force/Program.cs
@@ -1,1 +1,36 @@
+﻿using System;
 
+namespace ConsoleApp
+{
+    public class Program
+    {
+        // Naive method to find a pair in an array with a given sum
+        public static void FindPair(int[] A, int target)
+        {
+            // consider each element except the last
+            for (var i = 0; i < A.Length - 1; i++)
+            {
+                // start from the i'th element until the last element
+                for (var j = i + 1; j < A.Length; j++)
+                {
+                    // if the desired sum is found, print it
+                    if (A[i] + A[j] == target)
+                    {
+                        Console.WriteLine("Pair found (" + A[i].ToString() + ", " + A[j].ToString() + ")");
+                        return;
+                    }
+                }
+            }
+            // we reach here if the pair is not found
+            Console.WriteLine("Pair not found");
+        }
+
+        public static void Main(String[] args)
+        {
+            int[] arr = {8 , 7 , 2 , 5 , 3 , 1};
+            var target = 10;
+            FindPair(arr, target);
+            Console.ReadKey();
+        }
+    }
+}
